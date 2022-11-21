89a89dddde39aad8e8ff03291e9277a5363f4f83
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-11-21 10:58:54 +0100

    Minor changes

--- src/download_data.py
+++ src/download_data.py
@@ -6,8 +6,11 @@ import numpy as np
 import openfoodfacts
 import requests
 
+def print_log(text: str) -> None: print(f"[log] {text}")
+def print_error(text: str) -> None: print(f"[error] {text}")
+
 def main() -> None:
-    
+
 
 if __name__ == "__main__":
     main()
