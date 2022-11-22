ab28dc98e67ec616dcb373b025751e077359c8dc
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-11-22 17:42:29 +0100

    Minor update

--- src/download_data.py
+++ src/download_data.py
@@ -68,7 +68,7 @@ def main(total_data_points: int = 100, category_name: str = "Plant-based foods")
     # Default configuration
     parent_dir = abspath(join(dirname(abspath(__file__)), pardir))
     data_dir = join(parent_dir, 'data')
-    output_file = join(data_dir, 'products1.csv')
+    output_file = join(data_dir, 'products.csv')
 
     column_mapping = columns_mapping()
     required_columns = [x.split(".")[0] for x in list(column_mapping.values())]
