a7875cece5179fac786e2227659dbb1ef607e417
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-11-22 17:40:24 +0100

    Minor change

--- src/download_data.py
+++ src/download_data.py
@@ -43,6 +43,7 @@ def fetch_products(category: str, column_mapping: dict, required_columns: list, 
     check_keys = ["is_beverage", "negative_points", "energy_points", "saturated_fat_points", "sugars_points", "sodium_points", "positive_points", "fiber_points", "proteins_points", "fruits_vegetables_nuts_colza_walnut_olive_oils_points"]
     for _product in openfoodfacts.products.get_all_by_category(category):
         invalid = not check_required_columns(_product, required_columns)
+        # all_columns = list(_product.keys())
         # invalid = False
         # for c in required_columns:
         #     if c not in all_columns: invalid = True
